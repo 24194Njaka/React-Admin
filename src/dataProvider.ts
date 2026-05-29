b5d0abfaaac4c7debb0984bbb9832016ea25cf9d
Njaka <hei.herinjaka@gmail.com>
2026-05-29 12:49:53 +0300

    feat: add db

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,7 +1,6 @@
 import jsonServerProvider from "ra-data-json-server";
 
 // URL directe de l'API simulée (json-server)
-const API_URL = "http://localhost:3002";
-
+const API_URL = "http://127.0.0.1:3002";
 // Export du dataProvider
 export const dataProvider = jsonServerProvider(API_URL);
